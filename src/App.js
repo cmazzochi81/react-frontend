ab8ca012919652e96e12adfd2a257b97909c0aa5
Chris Mazzochi <cmazzochi81@gmail.com>
2022-06-28 17:36:26 -0600

    improving

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ export const App = () => {
                 <ForgotPasswordPage />
               </Route>
 
-              <Route path="/password-reset">
+              <Route path="/reset">
                 <PasswordResetLandingPage />
               </Route>
 
